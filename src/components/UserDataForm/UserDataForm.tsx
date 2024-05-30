83dcba88b1d39880675a579ed39aec9e62750d7d
Vishmay Chauhan <vishmay@helloar.io>
2024-05-30 18:55:37 +0530

    code cleanup

--- src/components/UserDataForm/UserDataForm.tsx
+++ src/components/UserDataForm/UserDataForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { TextField, Button, Typography, Grid } from '@mui/material';
 import './UserDataForm.css';
 
